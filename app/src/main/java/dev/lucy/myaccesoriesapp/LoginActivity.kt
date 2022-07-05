142f3e041222bb642d8fea58269c42ec08557a5c
L-Karimi <luciahkei101@gmail.com>
2022-07-05 11:35:51 +0300

    added viewbinding

--- app/src/main/java/dev/lucy/myaccesoriesapp/LoginActivity.kt
+++ app/src/main/java/dev/lucy/myaccesoriesapp/LoginActivity.kt
@@ -1,14 +1,39 @@
 package dev.lucy.myaccesoriesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.lucy.myaccesoriesapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var binding:ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.btnLogin.setOnClickListener {
+
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.tvNext.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        fun validateLogin() {
+            val name = binding.tietName.text.toString()
+            val password = binding.tietPasswroed.text.toString()
+            if (name.isBlank()) {
+                binding.tietName.error = "Username is required"
+            }
+            if (password.isBlank()) {
+                binding.tietPasswroed.error = "Username is required"
+            }
+        }
     }
+
 }
